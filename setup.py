5f6f5dd065ef1b187beb75d8689263433834f98c
Michael J. Pedersen <m.pedersen@icelus.org>
2009-12-06 23:13:42 -0500

    Initial import from paster template.

---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name='tgext.menu',
+    version='0.1',
+    description='',
+    author='',
+    author_email='',
+    #url='',
+    install_requires=[
+        "TurboGears2 >= 2.0b7",
+        "zope.sqlalchemy >= 0.4 ",
+        "repoze.tm2 >= 1.0a4",
+        ],
+    setup_requires=["PasteScript >= 1.7"],
+    paster_plugins=['PasteScript', 'Pylons', 'TurboGears2', 'tg.devtools'],
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['tgext'],
+    include_package_data=True,
+    test_suite='nose.collector',
+    tests_require=['WebTest', 'BeautifulSoup'],
+    message_extractors={'tgextmenu': [
+            ('**.py', 'python', None),
+            ('templates/**.mako', 'mako', None),
+            ('templates/**.html', 'genshi', None),
+            ('public/**', 'ignore', None)]},
+    entry_points="""
+    
+    """,
+)
