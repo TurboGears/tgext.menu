baed84e17f4d6ece411ded058c20c9138e706fc1
Michael J. Pedersen <m.pedersen@icelus.org>
2010-01-13 23:00:29 -0500

    This should complete setup.py. Testing.

--- setup.py
+++ setup.py
@@ -8,20 +8,20 @@ except ImportError:
 
 setup(
     name='tgext.menu',
-    version='0.1',
-    description='',
-    author='',
-    author_email='',
-    #url='',
+    version='0.2',
+    description='Automatic menu/navbar/sidebar generation extension for TurboGears',
+    author='Michael Pedersen',
+    author_email='m.pedersen@icelus.org',
+    url='http://bitbucket.org/pedersen/tgext.menu',
     install_requires=[
-        "TurboGears2 >= 2.1a3",
+        "TurboGears2 >= 2.1a2",
         "repoze.tm2 >= 1.0a4",
         'Mako >= 0.2.4',
         'tw.jquery'
         ],
     setup_requires=["PasteScript >= 1.7"],
     paster_plugins=['PasteScript', 'Pylons', 'TurboGears2', 'tg.devtools'],
-    packages=find_packages(exclude=['ez_setup']),
+    packages=find_packages(exclude=['ez_setup', "*.test", "*.test.*", "test.*", "test"]),
     namespace_packages=['tgext'],
     include_package_data=True,
     test_suite='nose.collector',
@@ -31,5 +31,6 @@ setup(
             ('templates/**.mako', 'mako', None),
             ('templates/**.html', 'genshi', None),
             ('public/**', 'ignore', None)]},
-    package_data={'tgext.menu' : ['static/*', 'templates/*'] }
+    package_data={'tgext.menu' : ['static/*', 'templates/*'] },
+    dependency_links=['http://www.turbogears.org/2.1/downloads/']
 )
